8ffdc2705b6e4fcb2dea363a9530b1b80ac9699e
Michael <michi_d@gmx.net>
2022-07-11 18:10:17 +0200

    Refactor the python unittests for working github actions

--- test/experiment/approaches/test_pddl.py
+++ test/experiment/approaches/test_pddl.py
@@ -2,17 +2,20 @@ import unittest
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', '..')))
-from src.experiment.approaches.pddl import interpret_sas_plan
+from src.experiment.approaches.pddl_solver import interpret_sas_plan
 
 class TestUtils(unittest.TestCase):
 
     def test_interpret_sas_plan(self):
         filename = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sas_plan'))
-        
-        opt_value, order = interpret_sas_plan(filename)
 
-        self.assertEqual(opt_value, 17)
-        self.assertEqual(order, ['20001', '10014', '10012', '50013'])
+        with open(filename, 'r') as filehandle:
+            cmd_output = filehandle.read()
+
+            opt_value, order = interpret_sas_plan(cmd_output)
+
+            self.assertEqual(opt_value, 17)
+            self.assertEqual(order, ['20001', '10014', '10012', '50013'])
 
 if __name__ == '__main__':
     unittest.main()
